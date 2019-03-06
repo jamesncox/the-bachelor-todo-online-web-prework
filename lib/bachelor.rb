d3fc76e3ae885b9fd03b99140c42bf517069234e
James Cox <jamesncox@yahoo.com>
2019-03-06 23:47:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,7 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, contestants|
     contestanst.each do |contestant_hash|
-      
+      if contestant_hash["name"]
     
     
 end
