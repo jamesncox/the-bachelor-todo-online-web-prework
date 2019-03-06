e3b2395e9f8b3eb8e8ee12bb831a0d563bec5ba6
James Cox <jamesncox@yahoo.com>
2019-03-06 23:46:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,8 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data.each do |contestanta, data|
+  data.each do |season, contestants|
+    contestanst.each do |
     
     
 end
